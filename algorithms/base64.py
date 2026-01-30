74b2db06ec88dc1fe870b485b6f343d40572e4ac
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2026-01-29 19:01:30 -0600

    Update base64.py

--- algorithms/base64.py
+++ algorithms/base64.py
@@ -1,15 +1,22 @@
 from binary import ascii_to_bin, bin_to_ascii
 
-###### made with ChatGPT 4o ######
+
 base64_chars: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 base64_dict: dict[int, str] = {i: ch for i, ch in enumerate(base64_chars)}
-###### ------------------- #######
+base64_dict_swapped: dict[str, int] = {ch: i for i, ch in enumerate(base64_chars)}
 
 
-def binary_to_base64(input: str) -> list:
+def binary_to_base64(input_bin: str) -> str:
     arr: list = []
-    for i in input:
-        arr.append(ascii_to_bin(ord(i)))
+    for ch in input_bin:
+        if ch == " ":
+            continue
+        else:
+            arr.append(ch)
+
+    if len(arr) % 8 != 0:
+        return "Error: bin number is not divisible by 8"
+
     text: str = ''.join(arr)
     arr = []
     chunk: list = []
@@ -19,33 +26,50 @@ def binary_to_base64(input: str) -> list:
         if len(chunk) == 6:
             arr.append(''.join(chunk))
             chunk = []
+
     # fill last chunk
+    print("chunk before", chunk)
     if chunk:
+        padding: str = "00111101" # bin for '='
         while len(chunk) < 6:
             chunk.append('0')
+
         arr.append(''.join(chunk))
-    
+    print("chunk after", chunk)
+
+
+
 
     final_arr: list = []
 
     for i, a in enumerate(arr):
-        final_arr.append(bin_to_ascii(a))
+        final_arr.append(base64_dict.get(bin_to_ascii(a), "?"))
+
+
     # TODO missing padding
-    return final_arr
+    return ''.join(final_arr)
 
 
-def base64_to_binary(input: str) -> str:
+def base64_to_binary(input_base64: str) -> list:
     arr: list = []
-    raise NotImplementedError()
+    for char in input_base64:
+        arr.append(ascii_to_bin(base64_dict_swapped.get(char, 0)))
+    return arr
+
+
+def base64_to_ascii(input_base64: str) -> list:
+    arr: list = base64_to_binary(input_base64=input_base64)
+
+        
 
 if __name__ == '__main__':
     try: 
         mode: str = input("Ingrese modo: ")
         text_input: str = input("Ingrese texto: ")
         if mode == "encrypt":
-            print(char_to_base64(input=text_input))
+            print(binary_to_base64(input_bin=text_input))
         elif mode == "decrypt":
-            print(base64_to_char(input=text_input))
+            print(base64_to_binary(input_base64=text_input))
     except KeyboardInterrupt:
         print("Programa finalizado por usuario")
     else:
