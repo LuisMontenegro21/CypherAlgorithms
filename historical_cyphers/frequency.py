fa8f1b8b7463657f5fee144c07bb2e3c1787162e
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2026-01-31 19:13:37 -0600

    Update frequency.py

--- historical_cyphers/frequency.py
+++ historical_cyphers/frequency.py
@@ -1,5 +1,30 @@
 
 
 
-def frecuency_analysis(text: str) -> None:
-    return 
+
+
+def frequency_analysis(text: str) -> dict:
+    relative_freq_dict: dict[str, float] = {}
+    letter_freq: dict[str, int] = {} # count how many times a letter is found throughout the string
+    seen: set = set()
+    for t in text:
+        if t not in seen:
+            seen.add(t)
+            letter_freq[t] = 1
+        else:
+            letter_freq[t] = letter_freq.get(t, 0) + 1
+    
+    for letter in seen:
+        relative_freq_dict[letter] = letter_freq.get(letter, 1) / len(text)
+        
+    return relative_freq_dict
+
+
+if __name__ == '__main__':
+    try: 
+        text: str = input("Ingrese texto: ")
+        print(f"Frequencias del texto: {[f'{k} : {v:.2f}' for k, v in frequency_analysis(text=text).items()]}")
+    except KeyboardInterrupt:
+        print("Programa finalizado por usuario")
+    else:
+        print("Programa finalizado")
